e925c98ba69a73827fc49d3a638fe40385ae7b5c
Aleks <gostuding>
2023-05-10 22:21:15 +0300

    iter5 done

--- cmd/server/handlers/handlers.go
+++ cmd/server/handlers/handlers.go
@@ -14,17 +14,17 @@ func PathNotFound(writer http.ResponseWriter, request *http.Request) {
 	fmt.Printf("Bad geteway: '%s', path: '%s'\r\n", request.Method, request.URL.Path)
 }
 
+// Отправка ответа, что данный метод не доступен для этого адреса
 func MethosNotAllowed(writer http.ResponseWriter, request *http.Request) {
 	fmt.Printf("Method not allowed: method: '%s', path: '%s'\r\n", request.Method, request.URL.Path)
 	writer.WriteHeader(http.StatusMethodNotAllowed)
-	writer.Write([]byte("Method not allowed. User POST method instead"))
+	writer.Write([]byte("Method not allowed. Use other method instead"))
 }
 
 // Обработка запроса на добавление или изменение метрики
 func Update(writer http.ResponseWriter, request *http.Request, storage storage.StorageSeter) {
 
-	status, err := storage.Update(chi.URLParam(request, "m_type"),
-		chi.URLParam(request, "m_name"), chi.URLParam(request, "m_value"))
+	status, err := storage.Update(chi.URLParam(request, "m_type"), chi.URLParam(request, "m_name"), chi.URLParam(request, "m_value"))
 	writer.WriteHeader(status) // запись статуса для возврата
 	if err != nil {
 		fmt.Println(err)
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,12 +4,18 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 
-	ip_address := flag.String("a", ":8080", "address and port to run server")
+	ip_address := flag.String("a", ":8080", "address and port to run server like address:port")
 	flag.Parse()
+	//-------------------------------------------------------------------------
+	if address := os.Getenv("ADDRESS"); address != "" {
+		ip_address = &address
+	}
+	//-------------------------------------------------------------------------
 	fmt.Println("Run server at adress: ", *ip_address)
 	err := http.ListenAndServe(*ip_address, GetRouter())
 	if err != nil {
